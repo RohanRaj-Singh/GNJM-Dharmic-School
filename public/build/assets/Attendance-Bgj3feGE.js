3a2f5177f95c66dbd799883873f49d4a5c6b5bf7
RohanRaj-Singh <mrsinghdev13@gmail.com>
2026-01-26 04:57:56 +0500

    New build after second update

---
+++ public/build/assets/Attendance-Bgj3feGE.js
@@ -0,0 +1,1 @@
+import r from"./Dashboard-C4B8ZS37.js";import"./app-CQFX0d8W.js";import"./SimpleLayout-eHP2zkYo.js";export{r as default};
---
+++ public/build/assets/Reports-S7DEkxoF.js
@@ -0,0 +1,1 @@
+import{j as e}from"./app-CQFX0d8W.js";import{S as r}from"./SimpleLayout-eHP2zkYo.js";function s(){return e.jsx(r,{title:"Reports",children:e.jsx("div",{className:"bg-white rounded-xl shadow p-6",children:"Reports will be here"})})}export{s as default};
