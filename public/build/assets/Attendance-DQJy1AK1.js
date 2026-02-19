1ae73c5fe2f503f1add361cffc2c14edfd00af9d
RohanRaj-Singh <mrsinghdev13@gmail.com>
2026-02-19 19:31:09 +0500

    Attendance fixed, frontend commit

--- public/build/assets/Attendance-DQJy1AK1.js
+++
@@ -1,1 +0,0 @@
-import r from"./Dashboard-DAkzlQKL.js";import"./app-B4j9P2OD.js";import"./SimpleLayout-SMPOUax8.js";export{r as default};
---
+++ public/build/assets/Attendance-TNNoy5Tr.js
@@ -0,0 +1,1 @@
+import r from"./Dashboard-D4oqXD3W.js";import"./app-DTPqrqzE.js";import"./SimpleLayout-wfPLQVAw.js";export{r as default};
