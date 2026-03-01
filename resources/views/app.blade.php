0d252760dc044be0b7a066855913aa0604ad86fc
RohanRaj-Singh <mrsinghdev13@gmail.com>
2026-03-01 21:02:26 +0500

    Website and App icon set

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,8 +4,11 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="theme-color" content="#ffffff">
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
         <link rel="icon" type="image/png" href="{{ Vite::asset('resources/images/logo.png') }}">
+        <link rel="apple-touch-icon" href="{{ Vite::asset('resources/images/logo.png') }}">
+        <link rel="manifest" href="/manifest.json">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
