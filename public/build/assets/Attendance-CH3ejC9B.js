2a489e9afad42090ed3e833c8ff430991b121d32
RohanRaj-Singh <mrsinghdev13@gmail.com>
2026-07-17 00:31:11 +0500

    feat: add transition management and role-based access hook
    - Introduced a new transition management module in `transition-Cm_bzlYm.js` to handle client-server transitions and animation states.
    - Added a role-based access hook in `useRoles-DTBtnEHc.js` to determine user roles and permissions (admin, accountant, teacher).

--- public/build/assets/Attendance-CH3ejC9B.js
+++
@@ -1,1 +0,0 @@
-import r from"./Dashboard-ZeJ4abkU.js";import"./app-BmK-wHBT.js";import"./SimpleLayout-OIhF6_YV.js";import"./TabSessionTimeout-CqS7TlwL.js";export{r as default};
---
+++ public/build/assets/InputLabel-BXYu_V3J.js
@@ -0,0 +1,1 @@
+import{j as m}from"./app-C6jX-qR3.js";function o({value:t,className:e="",children:r,...s}){return m.jsx("label",{...s,className:"block text-sm font-medium text-gray-700 "+e,children:t||r})}export{o as I};
