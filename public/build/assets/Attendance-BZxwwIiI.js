47e881aa6655c2cf7f643d7b5b353ce235e94418
RohanRaj-Singh <mrsinghdev13@gmail.com>
2026-07-27 15:06:03 +0500

    build ran

---
+++ public/build/assets/Attendance-BZxwwIiI.js
@@ -0,0 +1,1 @@
+import r from"./Dashboard-C5Ng6EON.js";import"./app-UnJbTTxz.js";import"./SimpleLayout-CEoaP5d5.js";import"./TabSessionTimeout-B2q5KfyW.js";export{r as default};
--- public/build/assets/Attendance-DmgTE2A0.js
+++
@@ -1,1 +0,0 @@
-import r from"./Dashboard-D23UkytM.js";import"./app-CvsbpRqN.js";import"./SimpleLayout-DO2YiHAS.js";import"./TabSessionTimeout-DiDyYNz3.js";export{r as default};
